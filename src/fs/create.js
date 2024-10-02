64a719e4763ae07e68428d098817b832bf7ea309
Dhruvin_Bhatt <dhruvin@tiqniat.com>
2024-10-02 18:37:33 +0530

    add create.js changes

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,39 @@
+import fs from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = './src/fs/files/fresh.txt';
+  
+  const fileExists = async () => {
+    try {
+      await fs.access(filePath);
+      return true;  
+    } catch (error) {
+      if (error.code === 'ENOENT')  // File does not exist
+        return false;
+      throw error;  
+    }
+  };
+
+  const writeFile = async () => {
+    try {
+      await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' }); //wx prevents data loss
+      console.log('File has been created successfully');
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const exists = await fileExists();
+  if (exists) {
+    throw new Error('FS operation failed');
+  }
+  await writeFile();
 };
 
-await create();
+(async () => {
+  try {
+    await create();
+  } catch (error) {
+    console.error(error.message);
+  }
+})();
